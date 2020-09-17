c7dd5ea2506a67a20a5fb98855b4bf6d29e224e8
Nick Guerra <nickg@maliciousgames.com>
2020-09-17 07:58:27 -0500

    added dynamic input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 class App extends Component {
   state = {
@@ -10,7 +12,19 @@ class App extends Component {
     this.setState({userInput: event.target.value});
   };
 
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+
+    this.setState({userInput: updatedText});
+  };
+
   render() {
+    const charList = this.state.userInput.split('').map((char, index) => {
+      return <Char character={char} key={index} clicked={() => this.deleteCharHandler(index)} />;
+    });
+
     return (
       <div className='App'>
         <input
@@ -19,6 +33,8 @@ class App extends Component {
           value={this.state.userInput} // change state of userInput 2 way binding
         />
         <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
+        {charList}
       </div>
     );
   }
